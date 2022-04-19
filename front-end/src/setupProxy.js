e8a8874d678b6934a74ca34d8cfd13fbb3fbf36d
oldweeb <maksym.pavliv.pz.2020@lpnu.ua>
2022-04-19 19:16:38 +0300

    front-end project added.

---
+++ front-end/src/setupProxy.js
@@ -0,0 +1,12 @@
+﻿const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+    app.use(
+        '/api',
+        createProxyMiddleware({
+            target: 'http://localhost:5249',
+            secure: false,
+            changeOrigin: true
+        })
+    );
+};
